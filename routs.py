30c0cc2aaf816bacceffc3836e75885d21c7d7bc
YanochkaM <140158192+YanochkaM@users.noreply.github.com>
2023-07-27 13:23:41 +0300

    Helper pull

--- routs.py
+++ routs.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from views import AdView
 
-app = Flask("app")
+app = Flask("app")#что означает app?
 
 app.add_url_rule("/advertisements/<int:id_ad>/", view_func=AdView.as_view('advertisements_delete'),
-                 methods=['DELETE', 'GET'])
-app.add_url_rule("/advertisements", view_func=AdView.as_view('advertisements_create'), methods=['POST'])
+                 methods=['DELETE', 'GET'])#когда ставим круглые скобки, а когда квадратные? в чем разница
+app.add_url_rule("/advertisements", view_func=AdView.as_view('advertisements_create'), methods=['POST'])#что означает method Post?
